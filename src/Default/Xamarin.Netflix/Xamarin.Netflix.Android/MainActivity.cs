aecb2c5a22e9f2f829635475ea349f05c9a03aef
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-10-26 21:28:39 +0200

    Added code

---
+++ src/Default/Xamarin.Netflix/Xamarin.Netflix.Android/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using FFImageLoading.Forms.Droid;
+using Xamarin.Netflix.ViewModels.Base;
+using FormsToolkit;
+
+namespace Xamarin.Netflix.Droid
+{
+    [Activity (Label = "Xamarin.Netflix", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.ToolbarLogo;
+
+            base.OnCreate(bundle);
+
+            CachedImageRenderer.Init();
+
+            Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+
+            // Remove the logo when we're not on the main page.
+            MessagingService.Current.Subscribe<bool>(MessageKeys.ChangeToolbar, (page, showLogo) =>
+            {
+                var logo = FindViewById<ImageView>(Resource.Id.logoImageLayout);
+
+                if (showLogo)
+                {
+                    logo.Visibility = ViewStates.Visible;
+                }
+                else
+                {
+                    logo.Visibility = ViewStates.Invisible;
+                }
+            });
+        }
+    }
+}
---
+++ src/Default/Xamarin.Netflix/Xamarin.Netflix/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using DLToolkit.Forms.Controls;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Netflix.Services.Navigation;
+using Xamarin.Netflix.ViewModels.Base;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Xamarin.Netflix
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            InitPlugins();
+            InitNavigation();
+        }
+
+        private void InitPlugins()
+        {
+            FlowListView.Init();
+        }
+
+        private Task InitNavigation()
+        {
+            var navigationService = Locator.Instance.Resolve<INavigationService>();
+            return navigationService.InitializeAsync();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
